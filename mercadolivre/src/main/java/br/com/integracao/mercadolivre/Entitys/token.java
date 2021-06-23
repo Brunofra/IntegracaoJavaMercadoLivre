5d30f260e7e96cf80be68d8a8385ff735080f025
Bruno Alves <brunoalvesfra@gmail.com>
2021-06-23 04:27:58 -0300

    conexão com base de dados

---
+++ mercadolivre/src/main/java/br/com/integracao/mercadolivre/Entitys/token.java
@@ -0,0 +1,28 @@
+package br.com.integracao.mercadolivre.Entitys;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+public @Data class token implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7307650567657994344L;
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	private Long id;
+	private String clientId;
+	private String clientSecret;
+	private String redirectUri;
+	private String sysPassword;
+	private String token;
+
+}

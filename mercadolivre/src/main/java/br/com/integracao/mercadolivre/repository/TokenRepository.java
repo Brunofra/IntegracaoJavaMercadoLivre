ada5d9af074416eae8a16f29d6b0afa4f2a8329a
Bruno Alves <brunoalvesfra@gmail.com>
2021-06-23 21:37:21 -0300

    delete de arquivos

--- mercadolivre/src/main/java/br/com/integracao/mercadolivre/repository/TokenRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.integracao.mercadolivre.repository;
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.integracao.mercadolivre.Models.Token;
-
-public interface TokenRepository extends CrudRepository<Token, String> {
-	Iterable<Token> finByToken(Token token);
-	Token findById(Long id);
-}

e593106224c79b34a2b8baafce294180fb0366d9
Bruno Alves <brunoalvesfra@gmail.com>
2021-06-25 12:45:09 -0300

    - token e refresh_token prontos

--- mercadolivre/src/main/java/com/br/integracao/mercadolivre/controller/AcessoController.java
+++ mercadolivre/src/main/java/com/br/integracao/mercadolivre/controller/AcessoController.java
@@ -1,12 +1,12 @@
 package com.br.integracao.mercadolivre.controller;
 
-import java.lang.ProcessBuilder.Redirect;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -19,25 +19,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 import com.br.integracao.mercadolivre.input.AcessoIn;
+import com.br.integracao.mercadolivre.input.Tokenin;
 import com.br.integracao.mercadolivre.models.Acesso;
+import com.br.integracao.mercadolivre.models.Token;
 import com.br.integracao.mercadolivre.output.AcessoOut;
 import com.br.integracao.mercadolivre.repository.AcessoRepository;
+import com.br.integracao.mercadolivre.repository.TokenRepository;
 
 @Controller
 @RequestMapping("/start/*")
 public class AcessoController {
 
 	@Autowired
-	private AcessoRepository er;
+	private AcessoRepository acesso;
+	@Autowired
+	private TokenRepository token;
 
 	@GetMapping("/token")
 	@ResponseBody
 
 	public AcessoOut gettoken(@RequestBody AcessoIn id) throws Exception {
-		Acesso acesso = er.findById(id.getId());
-		RestTemplate redirect = new RestTemplate();
-		ResponseEntity<String> get = redirect.exchange(acesso.getLinkAutorizacao(), HttpMethod.GET, null, String.class);
-		String code = get.getBody();
+		Acesso acesso = this.acesso.findById(id.getId());
+		//RestTemplate redirect = new RestTemplate();
+		//ResponseEntity<String> get = redirect.exchange(acesso.getLinkAutorizacao(), HttpMethod.GET, null, String.class);
+		//String code = get.getBody();
 		//
 		HttpHeaders header = new HttpHeaders();
 		header.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
@@ -57,12 +62,54 @@ public class AcessoController {
 			AcessoOut out = new AcessoOut();
 			
 			out = response.getBody();
-			
+			Token token = new Token();
+			token.setTokenAcesso(out.getTokenAcesso());
+			token.setExpiracao(out.getExpiracao());
+			token.setEscopo(out.getEscopo());
+			token.setTipoToken(out.getTipoToken());
+			token.setUsuario(out.getUsuario());
+			token.setCodeAlterado(out.getCodeAlterado());
+			this.token.save(token);
 			return out;
 		}
 		else {
-			return null;
+			Tokenin to = new Tokenin();
+			to.setId(id.getId());			
+			return refresh(to);
 		}
 
 	}
+	
+	
+	@GetMapping("/refreshtoken")
+	@ResponseBody
+	public AcessoOut refresh(@RequestBody Tokenin token) throws Exception{
+		Token to = this.token.findById(token.getId());
+		Acesso ac = this.acesso.findById(token.getId());
+		Map<String, String> mapa = new HashMap<>();
+		mapa.put("grant_type","refresh_token");
+		mapa.put("client_id", ac.getAppId());
+		mapa.put("client_secret", ac.getSenhaUsuario());
+		mapa.put("refresh_token",to.getCodeAlterado());
+		RestTemplate rest = new RestTemplate();
+		ResponseEntity<AcessoOut> response = rest.postForEntity("https://api.mercadolibre.com/oauth/token", mapa, AcessoOut.class);
+		if(response.hasBody()) {
+			AcessoOut out = new AcessoOut();
+			
+			out = response.getBody();
+			Token tokensave = new Token();
+			tokensave.setTokenAcesso(out.getTokenAcesso());
+			tokensave.setExpiracao(out.getExpiracao());
+			tokensave.setEscopo(out.getEscopo());
+			tokensave.setTipoToken(out.getTipoToken());
+			tokensave.setUsuario(out.getUsuario());
+			tokensave.setCodeAlterado(out.getCodeAlterado());
+			this.token.save(tokensave);
+			return out;
+		}else {
+			return null;
+		}
+	}
+	
+	
 }
--- mercadolivre/src/main/java/com/br/integracao/mercadolivre/models/Chamadas.java
+++ mercadolivre/src/main/java/com/br/integracao/mercadolivre/models/Chamadas.java
@@ -1,41 +1,30 @@
 package com.br.integracao.mercadolivre.models;
 
+import lombok.Data;
 import java.io.Serializable;
 
+import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Data;
-
 @Entity
-@Table (name = "acesso")
-public @Data class Acesso implements Serializable{
-
-	/**
+@Table(name = "chamadas")
+public @Data class Chamadas implements Serializable{/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	
-
 	private long id;
 	
+	private String descricao;
 	
-	private String appId; 
-
-	
-	private String code; 
+	private String linkchamada;
 	
 	
-	private String redirecionamento;
-	
-	
-	private String senhaUsuario;
-	
-	private String grantType;
 
 }
